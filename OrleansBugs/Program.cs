068fdc733a70313389b6a8645a9e5b359ac36f60
slango0513 <tong.zhi.1989@gmail.com>
2019-04-15 08:12:12 +0800

    添加项目文件。

---
+++ OrleansBugs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OrleansBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
